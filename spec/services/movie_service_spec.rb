a4d116e6a6e6a35b9f09cc2f135212054abbea2c
Kevin Gloss <kgloss89@gmail.com>
2022-02-02 10:34:29 -0700

    Test api top rated movies

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -4,9 +4,9 @@ RSpec.describe 'Movie Service' do
     describe 'class methods' do 
         it 'returns the top rated movies', :vcr do 
             service = MovieService.new
-            
+            yml = File.read('spec/fixtures/vcr_cassettes/movie_service/class_methods/returns_the_top_rated_movies.yml')
             expect(service.top_rated_movies).to be_a(Hash)
-            expect(service.top_top_movies[:results][0][:title]).to eq("Your Eyes Tell")
+            expect(service.top_rated_movies[:results][0][:title]).to eq("Your Eyes Tell")
         end
     end
 end
