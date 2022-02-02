1c2744cba690bb9073bcac8ac50bdb35c6714308
Kevin Gloss <kgloss89@gmail.com>
2022-02-02 10:59:29 -0700

    Reformat for api call

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,21 +2,12 @@ class MoviesController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
+# binding.pry
+    @movies = facade.search_filter(params[:filter])
+    
+  end
 
-    # Next 3 lines work
-    # conn = Faraday.new("https://api.themoviedb.org")
-    # response = conn.get("/3/movie/top_rated?api_key=09ebf0fa4e1ae6359be80bb97f58a428&page=1")
-    # @top_movies = JSON.parse(response.body, symbolize_names: true)
-    
-    conn = Faraday.new("https://api.themoviedb.org/3/", params: {api_key: ENV['movie_api_key']})
-    
-    response = conn.get("movie/#{params[:filter]}/?language=en-US")
-    # response = conn.get('movie/top_rated?api_key=09ebf0fa4e1ae6359be80bb97f58a428&language=en-US&page=1')
-    data = JSON.parse(response.body, symbolize_names: true)
-    binding.pry
-    # results = data[:results]
-    # @movies = results.map do |data|
-    #    Movie.new(data)
-    # end
+  def facade 
+    MovieFacade.new
   end
 end
--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,7 +1,7 @@
 class MovieFacade 
     def search_filter(filter)
         if filter == 'top_rated'
-            # top_rated_movies
+            service.top_rated_movies
         elsif filter
             # put in service.search feature
         end
