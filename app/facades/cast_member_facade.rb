be8a1d8dbf51ae325ca3a9764d5a300af9f0a716
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 15:56:03 -0700

    Fix method name typo

--- app/facades/cast_member_facade.rb
+++ app/facades/cast_member_facade.rb
@@ -1,7 +1,7 @@
 class CastMemberFacade 
     class << self
         def starring(movie_id)
-            response = CastMemberService.cast(movie_id)
+            response = CastMemberService.starring(movie_id)
 
             response.map do |data|
                 CastMember.new(data)
