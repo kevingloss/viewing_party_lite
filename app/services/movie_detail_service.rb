29010f7bfb6f1d4263ae4211af06a890184266c2
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 14:21:06 -0700

    Remove unnecessary variables

--- app/services/movie_detail_service.rb
+++ app/services/movie_detail_service.rb
@@ -1,11 +1,8 @@
 class MovieDetailService < ApiService
     class << self 
         def find_movie(movie_id)
-            response = connection.get("/movie") do |faraday|
-                faraday.params['movie_id'] = movie_id
-            end
-            data = parse_json(response)
-            binding.pry
+            response = connection.get("/movie/#{movie_id}")
+            data = parse_json(response)[:results]
         end
     end
 end
