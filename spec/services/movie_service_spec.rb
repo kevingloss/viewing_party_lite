40614d9a6a23901878a098e499da57a3f2bf644c
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 08:50:20 -0700

    Remove commented out code

--- spec/services/movie_service_spec.rb
+++ spec/services/movie_service_spec.rb
@@ -6,19 +6,16 @@ RSpec.describe 'Movie Service' do
           service = MovieService.top_rated_movies
           expect(service).to be_a(Hash)
           expect(service[:results][0][:title]).to eq("Your Eyes Tell")
-          # below expectation supposed to be 40. Fix later
-          expect(service[:results].count).to eq(20)
-
-            # service = MovieService.new
-            # binding.pry
-            # yml = File.read('spec/fixtures/vcr_cassettes/movie_service/class_methods/returns_the_top_rated_movies.yml')
-            # expect(service.top_rated_movies).to be_a(Array)
-            # expect(service.top_rated_movies[0][:title]).to eq("Your Eyes Tell")
+          expect(service[:results].count).to eq(40)
         end
 
-        it 'searches for movie', :vcr do
+        it 'searches for movies', :vcr do
           service = MovieService.search_title('The Godfather')
           expect(service[:results][0][:title]).to eq('The Godfather')
+        end
+
+        it 'finds a movie' do 
+
         end
     end
 end
