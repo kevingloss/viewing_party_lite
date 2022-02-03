1c613985072f57ffb67d764c85a1e5a56788b2c0
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 11:44:47 -0700

    Refactor movie facade

--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,25 +1,17 @@
 class MovieFacade
   class << self
     def find_top_rated_movies
-        service = MovieService.top_rated_movies
+        json = MovieService.top_rated_movies
 
-        service[:results].map do |data|
+        json[:results].map do |data|
             Movie.new(data)
         end
     end
 
     def search_movies(keyword)
-        service = MovieService.search_title(keyword)
+        json = MovieService.search_title(keyword)
         
-        service[:results].map do |data|
-            Movie.new(data)
-        end
-    end
-    
-    def movie_details(movie_id)
-        service = MovieService.find_movie(movie_id)
-        
-        service[:results].map do |data|
+        json[:results].map do |data|
             Movie.new(data)
         end
     end
