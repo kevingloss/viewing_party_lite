c2124ef432d64345e7154cb99e4e6c37475d5272
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 08:10:06 -0700

    Uncomment Code

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -2,17 +2,17 @@ class MoviesController < ApplicationController
 
   def index
     @user = User.find(params[:user_id])
-    @movies = facade.search_filter(params[:filter])
-    # binding.pry
+    if params[:filter] == 'top_rated'
+      @movies = MovieFacade.find_top_rated_movies
+      @page_title = 'Top Rated Movies'
+    elsif params[:filter]
+      @movies =  MovieFacade.search_movies(params[:filter])
+      @page_title = "Search results for: #{params[:filter]}"
+    end
   end
 
   def show
     @user = User.find(params[:user_id])
     @movie = facade.movie_details(params[:id])
   end
-
-  def facade
-    MovieFacade.new
-  end
-
 end
--- app/facades/movie_facade.rb
+++ app/facades/movie_facade.rb
@@ -1,11 +1,4 @@
 class MovieFacade
-    # def search_filter(filter)
-    #     if filter == 'top_rated'
-    #         find_top_rated_movies
-    #     elsif filter
-    #         search_movies(filter)
-    #     end
-    # end
 
     def self.find_top_rated_movies
       service = MovieService.top_rated_movies
@@ -15,19 +8,19 @@ class MovieFacade
       end
     end
 
-    # def search_movies(keyword)
-    #     service.search_title(keyword).map do |data|
-    #         Movie.new(data)
-    #     end
-    # end
-    #
-    # def movie_details(movie_id)
-    #   service.find_movie(movie_id).map do |data|
-    #     Movie.new(data)
-    #   end
-    # end
-
-    # def service
-    #     MovieService.new
-    # end
+    def search_movies(keyword)
+        service = MovieService.search_title(keyword)
+        
+        service[:results].map do |data|
+            Movie.new(data)
+        end
+    end
+    
+    def movie_details(movie_id)
+        service = MovieService.find_movie(movie_id)
+        
+        service[:results].map do |data|
+            Movie.new(data)
+        end
+    end
 end
