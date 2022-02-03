8282540ccd0873754e05b9ae50c7fee5f708b169
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 13:19:55 -0700

    Create cast member test and poro

---
+++ spec/poros/cast_member_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+RSpec.describe CastMember do
+
+  it 'has attributes' do
+    data = {
+      name: 'Ian McKellen',
+      character: 'Gandalf'
+    }
+
+    actor = CastMember.new(data)
+
+    expect(actor).to be_a(CastMember)
+    expect(actor.name).to eq('Ian McKellen')
+    expect(actor.character).to eq('Gandalf')
+  end
+end
