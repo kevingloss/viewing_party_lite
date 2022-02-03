a8500c1f7e35622743e494e04caf7e371f475e10
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 11:45:46 -0700

    Add potential limits for top_rated and search

--- app/services/movie_service.rb
+++ app/services/movie_service.rb
@@ -1,6 +1,5 @@
 class MovieService < ApiService
   class << self
-
     def top_rated_movies
       response = connection.get("/movie/top_rated")
       parse_json(response)
@@ -10,24 +9,29 @@ class MovieService < ApiService
         # while movies.count < 40
         #     response = connection.get("/movie/top_rated?page=#{page}")
         #     data = parse_json(response)
-        #     movies.push(data[:results]).flatten!
+        #     movies += (data[:results])
         #     page += 1
         # end
         # movies.take(40)
     end
 
-    # need to update to max 40 results (add page 1 & 2 together?)
     def search_title(keyword)
         response = connection.get("/search/movie") do |faraday|
             faraday.params['query'] = keyword
         end
         # binding.pry
-      parse_json(response)
-    end
-
-    def find_movie(movie_id)
-      response = connection.get("/movie/#{movie_id}")
-      parse_json(response)
+        data = parse_json(response)
+        
+# need to update to max 40 results (add page 1 & 2 together?)
+    #   if data[:total_results] <= 20 
+    #     movies = data[:results]
+    #   elsif data[:total_results] <= 40
+    #     response_2 = connection.get("/search/movie?page=1") do |faraday|
+    #         faraday.params['query'] = keyword
+    #     end
+    #     data_2 = parse_json(response_2)
+    #     movies = data[:results] + response_2[:results]
+    #   end
     end
   end
 end
