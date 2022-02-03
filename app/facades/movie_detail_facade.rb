49a46e05299309dd187a77ad1c99e5e05909162a
Kevin Gloss <kgloss89@gmail.com>
2022-02-03 14:19:04 -0700

    Fix typo

--- app/facades/movie_detail_facade.rb
+++ app/facades/movie_detail_facade.rb
@@ -1,9 +1,9 @@
 class MovieDetailFacade
     class << self
       def movie_details(movie_id)
-          json = MovieDetailService.find_movie(movie_id)
+          response = MovieDetailService.find_movie(movie_id)
           
-          json[:results].map do |data|
+          response.map do |data|
               MovieDetail.new(data)
           end
       end
