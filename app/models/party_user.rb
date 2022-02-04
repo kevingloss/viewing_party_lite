0bcae2156901a339f9b87efa15c8ee6c52e67e40
Kevin Gloss <kgloss89@gmail.com>
2022-02-04 00:06:30 -0700

    Add methods for invitees and party_movie

--- app/models/party_user.rb
+++ app/models/party_user.rb
@@ -3,4 +3,12 @@ class PartyUser < ApplicationRecord
   belongs_to :party
 
   enum status: {pending: 0, hosting: 1, invited: 2}
+
+  def self.attending(user) 
+    where(user_id: user.id)
+  end
+
+  def party_movie
+    MovieDetailFacade.movie_details(self.party.movie_id)
+  end
 end
