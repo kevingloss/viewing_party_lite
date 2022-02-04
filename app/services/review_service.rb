0a77e0b25330b9ee2a0414a7c0cd41e3b0912e12
Eldridge Turambi <eturambi95@gmail.com>
2022-02-03 17:02:07 -0700

    Updated RevewService to inherit from ApiService

--- app/services/review_service.rb
+++ app/services/review_service.rb
@@ -1,13 +1,4 @@
-class ReviewService
-  def self.connection
-    Faraday.new(url: 'https://api.themoviedb.org/3/') do |faraday|
-      faraday.params['api_key'] = ENV['movie_api_key']
-    end
-  end
-
-  def self.parse_json(response)
-    JSON.parse(response.body, symbolize_names: true)
-  end
+class ReviewService < ApiService
 
   def self.author_name(movie_id)
     response = connection.get("movie/#{movie_id}/reviews")
