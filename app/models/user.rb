894a10fda067acceec7181163cb18f632397f5d5
Kevin Gloss <kgloss89@gmail.com>
2022-02-16 14:38:45 -0700

    Add password validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,6 +3,8 @@
 class User < ApplicationRecord
   has_many :party_users
   has_many :parties, through: :party_users
+  has_secure_password 
+  validates_presence_of :password_digest
 
   validates_presence_of :name, :email
   validates :email, email: true, uniqueness: true
